6ba09d772ec1ba6560a2842175888a90f684556c
Guilherme <guipredebon@gmail.com>
2022-05-18 19:40:55 -0300

    Projeto pizzaria

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, FlatList, TextInput } from "react-native";
+
+const pizzas = [
+	{ tipo: "salgada", sabor: "Calabresa com azeitonas pretas" },
+	{ tipo: "salgada", sabor: "Da casa" },
+	{ tipo: "salgada", sabor: "Portuguesa" },
+	{ tipo: "salgada", sabor: "Marguerita" },
+	{ tipo: "doce", sabor: "Creme de avelã" },
+	{ tipo: "doce", sabor: "Brigadeiro" },
+	{ tipo: "doce", sabor: "Califórnia" },
+];
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [tipo, setTipo] = useState("salgada");
+	return (
+		<View style={styles.container}>
+			<TextInput
+				style={styles.input}
+				placeholder="Tipo de pizza"
+				onChangeText={setTipo}
+				value={tipo}
+			/>
+			<FlatList
+				data={pizzas.filter((pizza) => pizza.tipo === "salgada")}
+				renderItem={({ item }) => <Text style={styles.item}>{item.sabor}</Text>}
+			/>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+		marginTop: 10,
+	},
+	item: {
+		backgroundColor: "#99CCFF",
+		padding: 20,
+		marginVertical: 8,
+		marginHorizontal: 16,
+	},
+	input: {
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		padding: 10,
+	},
 });
